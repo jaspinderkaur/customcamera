8cd120ceeb3460c5a3ffe0d5d1612f603c8c7381
Jaspinder Kaur <jaspinder@Jaspinders-MacBook-Air.local>
2016-07-17 17:01:23 +0530

    Capture Image

--- app/src/main/java/com/example/customcamera/CameraPreview.java
+++ app/src/main/java/com/example/customcamera/CameraPreview.java
@@ -2,16 +2,19 @@ package com.example.customcamera;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.Size;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by jaspinder on 7/17/16.
  */
-public class CameraPreview  extends SurfaceView implements SurfaceHolder.Callback {
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
   private SurfaceHolder mHolder;
   private Camera mCamera;
 
@@ -20,7 +23,6 @@ public class CameraPreview  extends SurfaceView implements SurfaceHolder.Callbac
   public CameraPreview(Context context, Camera camera) {
     super(context);
     mCamera = camera;
-
     // Install a SurfaceHolder.Callback so we get notified when the
     // underlying surface is created and destroyed.
     mHolder = getHolder();
@@ -37,15 +39,55 @@ public class CameraPreview  extends SurfaceView implements SurfaceHolder.Callbac
     }
   }
 
-  public void surfaceDestroyed(SurfaceHolder holder) {
-    // empty. Take care of releasing the Camera preview in your activity.
+  public boolean isCameraReady() {
+    return mCamera != null;
+  }
+
+  public void setFlashMode(int mFlashMode) {
+    if (mCamera != null) {
+      Camera.Parameters mParameters = mCamera.getParameters();
+      if (mFlashMode == 0) {
+        mParameters.setFlashMode(Parameters.FLASH_MODE_AUTO);
+      } else if (mFlashMode == 1) {
+        mParameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+      } else {
+        mParameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+      }
+      mCamera.setParameters(mParameters);
+    }
+  }
+
+  public void setCamera(Camera camera) {
+    if (mCamera == camera) {
+      return;
+    }
+
+    stopPreviewAndFreeCamera();
+
+    mCamera = camera;
+
+    if (mCamera != null) {
+      List<Size> localSizes = mCamera.getParameters().getSupportedPreviewSizes();
+      //mSupportedPreviewSizes = localSizes;
+      requestLayout();
+
+      try {
+        mCamera.setPreviewDisplay(mHolder);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      // Important: Call startPreview() to start updating the preview
+      // surface. Preview must be started before you can take a picture.
+      mCamera.startPreview();
+    }
   }
 
   public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
     // If your preview can change or rotate, take care of those events here.
     // Make sure to stop the preview before resizing or reformatting it.
 
-    if (mHolder.getSurface() == null){
+    if (mHolder.getSurface() == null) {
       // preview surface does not exist
       return;
     }
@@ -53,7 +95,7 @@ public class CameraPreview  extends SurfaceView implements SurfaceHolder.Callbac
     // stop preview before making changes
     try {
       mCamera.stopPreview();
-    } catch (Exception e){
+    } catch (Exception e) {
       // ignore: tried to stop a non-existent preview
     }
 
@@ -65,8 +107,45 @@ public class CameraPreview  extends SurfaceView implements SurfaceHolder.Callbac
       mCamera.setPreviewDisplay(mHolder);
       mCamera.startPreview();
 
-    } catch (Exception e){
+    } catch (Exception e) {
       Log.d(TAG, "Error starting camera preview: " + e.getMessage());
     }
+    // Now that the size is known, set up the camera parameters and begin
+    // the preview.
+    Camera.Parameters parameters = mCamera.getParameters();
+    //parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+    requestLayout();
+    mCamera.setParameters(parameters);
+
+    // Important: Call startPreview() to start updating the preview surface.
+    // Preview must be started before you can take a picture.
+    mCamera.startPreview();
   }
+
+  public void surfaceDestroyed(SurfaceHolder holder) {
+    // Surface will be destroyed when we return, so stop the preview.
+    if (mCamera != null) {
+      // Call stopPreview() to stop updating the preview surface.
+      mCamera.stopPreview();
+    }
+  }
+
+  /**
+   * When this function returns, mCamera will be null.
+   */
+  private void stopPreviewAndFreeCamera() {
+
+    if (mCamera != null) {
+      // Call stopPreview() to stop updating the preview surface.
+      mCamera.stopPreview();
+
+      // Important: Call release() to release the camera for use by other
+      // applications. Applications should release the camera immediately
+      // during onPause() and re-open() it during onResume()).
+      mCamera.release();
+
+      mCamera = null;
+    }
+  }
+
 }
--- app/src/main/java/com/example/customcamera/CustomCameraActivity.java
+++ app/src/main/java/com/example/customcamera/CustomCameraActivity.java
@@ -1,15 +1,26 @@
 package com.example.customcamera;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
+import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ImageButton;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+
+import com.example.customcamera.helper.CameraHelper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by jaspinder on 7/17/16.
@@ -17,11 +28,51 @@ import android.widget.RelativeLayout;
 public class CustomCameraActivity extends AppCompatActivity implements OnClickListener {
 
   private CameraPreview mCameraPreview;
+  private ImageView mFlash;
+  private ImageView mGrid;
+  private ImageView mGallery;
+  private ImageView mCameraInfo;
+  private ImageView mCancel;
+  /**
+   * grid view parameters
+   */
+  private RelativeLayout mGridLayout;
+
+  private Camera mCamera;
+  /**
+   * Flag to avoid taking picture while a previous one is being saved.
+   */
+  private boolean mSafeToTakePicture = true;
+  private final String TAG = "CustomCameraActivity";
+  private int mFlashMode = 0;
+
+  private static final String FLASH_AUTO = "auto";
+  private static final String FLASH_ON = "on";
+  private static final String FLASH_OFF = "off";
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_custom_camera);
+
+    mCancel = (ImageView) findViewById(R.id.ivCancel);
+    mGallery = (ImageView) findViewById(R.id.ivGallery);
+    mFlash = (ImageView) findViewById(R.id.ivFlash);
+    mGrid = (ImageView) findViewById(R.id.ivGrid);
+    mCameraInfo = (ImageView) findViewById(R.id.ivCaptureInfo);
+    mGridLayout = (RelativeLayout) findViewById(R.id.rlGrid);
+    //initialize grid parameters
+    mHorizontalLine1 = findViewById(R.id.vHorizontalLine1);
+    mHorizontalLine2 = findViewById(R.id.vHorizontalLine2);
+    mHorizontalLine3 = findViewById(R.id.vHorizontalLine3);
+    mVerticalLine1 = findViewById(R.id.vVerticalLine1);
+    mVerticalLine2 = findViewById(R.id.vVerticalLine2);
+    //Set up click listeners
+    mGallery.setOnClickListener(this);
+    mCancel.setOnClickListener(this);
+    mFlash.setOnClickListener(this);
+    mGrid.setOnClickListener(this);
+    mCameraInfo.setOnClickListener(this);
   }
 
   @Override
@@ -31,9 +82,10 @@ public class CustomCameraActivity extends AppCompatActivity implements OnClickLi
   }
 
   private void startCamera() {
-    RelativeLayout cameraPreviewLayout = (RelativeLayout) findViewById(R.id.rlCameraPreview);
+    FrameLayout cameraPreviewLayout = (FrameLayout) findViewById(R.id.flCameraPreview);
     cameraPreviewLayout.removeAllViews();
-    mCameraPreview = new CameraPreview(this, getCameraInstance());
+    mCamera = getCameraInstance();
+    mCameraPreview = new CameraPreview(this, mCamera);
     cameraPreviewLayout.addView(mCameraPreview);
     findViewById(R.id.bButtonCapture).setOnClickListener(this);
   }
@@ -56,8 +108,117 @@ public class CustomCameraActivity extends AppCompatActivity implements OnClickLi
     int id = view.getId();
     switch (id) {
       case R.id.bButtonCapture:
-
+        if (mSafeToTakePicture) {
+          mSafeToTakePicture = false;
+          mCamera.takePicture(null, null, mPicture);
+        }
         break;
+      case R.id.ivCancel:
+        finish();
+        break;
+      case R.id.ivFlash:
+        if (mCameraPreview.isCameraReady() && mSafeToTakePicture) {
+          if (mFlash.getTag().equals(FLASH_AUTO)) {
+            mFlash.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.capture_flash_on));
+            mFlash.setTag(FLASH_ON);
+            mFlashMode = 1;
+          } else if (mFlash.getTag().equals(FLASH_ON)) {
+            mFlash.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.capture_flash_off));
+            mFlash.setTag(FLASH_OFF);
+            mFlashMode = 2;
+          } else {
+            mFlash.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.capture_flash_on_auto));
+            mFlash.setTag(FLASH_AUTO);
+            mFlashMode = 0;
+          }
+
+          mCameraPreview.setFlashMode(mFlashMode);
+        }
+        break;
+      case R.id.ivGrid:
+        if (mGrid.getTag().equals(FLASH_ON)) {
+          mGrid.setTag(FLASH_OFF);
+          mGridLayout.setVisibility(View.GONE);
+          mGrid.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.capture_grid_off));
+        } else {
+          mGrid.setTag(FLASH_ON);
+          mGridLayout.setVisibility(View.VISIBLE);
+          mGrid.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.capture_grid_on));
+        }
+        break;
+      case R.id.ivGallery:
+        break;
+    }
+  }
+
+  private PictureCallback mPicture = new PictureCallback() {
+
+    @Override
+    public void onPictureTaken(byte[] data, Camera camera) {
+
+      File pictureFile = CameraHelper.getOutputMediaFile(CameraHelper.MEDIA_TYPE_IMAGE);
+      if (pictureFile == null) {
+        Log.d(TAG, "Error creating media file, check storage permissions");
+        return;
+      }
+
+      try {
+        FileOutputStream fos = new FileOutputStream(pictureFile);
+        fos.write(data);
+        fos.close();
+        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+        mGallery.setImageBitmap(bitmap);
+        mSafeToTakePicture = true;
+        try {
+          mCamera.stopPreview();
+          mCamera.setPreviewDisplay(null);
+        } catch (Exception e) {
+          // ignore: tried to stop a non-existent preview
+        }
+        if (safeCameraOpen(0)) {
+          startCamera();
+        }
+      } catch (FileNotFoundException e) {
+        Log.d(TAG, "File not found: " + e.getMessage());
+      } catch (IOException e) {
+        Log.d(TAG, "Error accessing file: " + e.getMessage());
+      }
+    }
+  };
+
+  private boolean safeCameraOpen(int id) {
+    boolean qOpened = false;
+
+    try {
+      releaseCameraAndPreview();
+      mCamera = Camera.open(id);
+      qOpened = (mCamera != null);
+    } catch (Exception e) {
+      Log.e(getString(R.string.app_name), "failed to open Camera");
+      e.printStackTrace();
+    }
+
+    return qOpened;
+  }
+
+  private void releaseCameraAndPreview() {
+    mCameraPreview.setCamera(null);
+    if (mCamera != null) {
+      mCamera.release();
+      mCamera = null;
+    }
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    releaseCamera();              // release the camera immediately on pause event
+  }
+
+  private void releaseCamera() {
+    if (mCamera != null) {
+      mCamera.release();        // release the camera for other applications
+      mCamera = null;
     }
   }
 }
